84dd928a284005219fedea8fcc4578f1edd239eb
pardeshim1999 <pardeshim1999@gmail.com>
2020-12-15 21:19:44 +0530

    Create relational.py

---
+++ python/relational.py
@@ -0,0 +1,32 @@
+a=10
+b=20
+c=0
+
+print"A=",a
+print"B=",b
+
+if(a==b):
+	print"If A is equal to B then statement is true:"
+else:
+	print"If A is not equal to B then statement is false:"
+if(a<>b):
+	print"If A is not eqal to B then statement is true"
+else:
+	print"If A is equal to B then statement is false"
+if(a>b):
+	print"If A is greater than B then statement is true"
+else:
+	print"If A is not greater than B then statement is false"
+if(a<b):
+	print"If B is greater than A then statement is true"
+else:
+	print"If B is not greater than A then statement is false"
+if(a>=b):
+	print"If A is greater than or equal to B then statement is true"
+else:
+	print"If A is not greater than equal to B then statement is false"
+if(a<=b):
+	print"If B is greater than or equal to A then statement is true"
+else:
+	print"If B is not greater than or equal to A then statement is false"
+
