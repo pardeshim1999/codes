518d458b6531619214edeff0a0003ff188f34de7
pardeshim1999 <pardeshim1999@gmail.com>
2020-12-15 21:16:33 +0530

    Create Test.java

---
+++ java/Test.java
@@ -0,0 +1,14 @@
+public class Test
+	{
+	public void pupage()
+		{
+			int age = 0;
+			age= age + 7;
+			System.out.println("Puppy age is: "+ age);
+		}
+	public  static void main( String agrs[])
+		{
+			Test test= new Test();
+			test.pupage();
+		}
+	}
