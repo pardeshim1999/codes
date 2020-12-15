1c10d154f094575d9c6b1ad02a3862ac5f7558f8
pardeshim1999 <pardeshim1999@gmail.com>
2020-12-15 21:19:28 +0530

    Create list.py

---
+++ python/list.py
@@ -0,0 +1,18 @@
+l1=(2011,2012,"manish","chaitanya")
+
+print("the list element is",l1)
+print("the list element is",l1[0])
+print("the list element is",l1[1])
+print("the list element is",l1[2])
+print("the list element is",l1[3])
+
+print("the list element is",l1[-1])
+print("the list element is",l1[-2])
+print("the list element is",l1[-3])
+print("the list element is",l1[-4])
+
+print("the list element is",l1[0:1])
+print("the list element is",l1[0:3])
+print("the list element is..",l1[-3:-1])
+
+print("the lenght of element is",len(l1))
