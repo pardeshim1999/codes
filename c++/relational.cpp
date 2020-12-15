6478c2265124b0198f674b2015a850dbd7c74351
pardeshim1999 <pardeshim1999@gmail.com>
2020-12-15 21:11:57 +0530

    Create relational.cpp

---
+++ c++/relational.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+	int main()
+	{
+		int a=12;
+		int b=12;
+
+		cout<<"A="<<a<<endl;
+		cout<<"B="<<b<<endl;
+
+		if(a==b)
+		{
+			cout<<"If (a==b) then statement is true"<<endl;
+		}
+		else
+		{
+			cout<<"If (a==b) then statement is wrong"<<endl;
+		}
+
+		if(a!=b)
+		{
+			cout<<"If (a!=b) then statement is true"<<endl;
+		}
+		else
+		{
+			cout<<"If (a!=b) then statement is wrong"<<endl;
+		}
+		if(a>b)
+		{
+			cout<<"If (a>b) then statement is true"<<endl;
+		}
+		else
+		{
+			cout<<"If (a>b) then statement is wrong"<<endl;
+		}
+		if(a<b)
+		{
+			cout<<"If (a<b) then statement is true"<<endl;
+		}
+		else
+		{
+			cout<<"If (a<b) then statement is wrong"<<endl;
+		}
+		if(a>=b)
+		{
+			cout<<"If (a>=b) then statement is true"<<endl;
+		}
+		else
+		{
+			cout<<"If (a>=b) then statement is wrong"<<endl;
+		}
+		if(a<=b)
+		{
+			cout<<"If (a<=b) then statement is true"<<endl;
+		}
+		else
+		{
+			cout<<"If (a<=b) then statement i false"<<endl;
+		}
+	return 0;
+	}
