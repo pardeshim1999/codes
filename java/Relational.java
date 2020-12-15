faf6ff803dda09adfbed6bdc409eeb70e550ef41
pardeshim1999 <pardeshim1999@gmail.com>
2020-12-15 21:16:00 +0530

    Create Relational.java

---
+++ java/Relational.java
@@ -0,0 +1,59 @@
+class Relational
+{
+	public static void main(String args[])
+	{
+	int a=10;
+	int b=12;
+	System.out.println("A=" + a);
+	System.out.println("B=" + b);
+	
+	if(a==b)
+		{
+			System.out.println("(a==b) is "+ (a==b));
+		}
+	else
+		{
+			System.out.println("(a==b) is "+ (a==b));
+		}
+	if(a!=b)
+		{
+			System.out.println("(a!=b) is "+ (a!=b));
+		}
+	else
+		{
+			System.out.println("(a!=b) is "+ (a!=b));
+		}
+	if(a>b)
+		{
+			System.out.println("(a>b) is: "+(a>b));
+		}
+	else
+		{
+			System.out.println("(a>b) is:"+(a>b));
+		}
+	if(a<b)
+		{
+			System.out.println("(a<b) is:  "+(a<b));
+		}
+	else
+		{
+			System.out.println("(a<b) is: "+(a<b));
+		}
+	if(a>=b)
+		{
+			System.out.println("(a>=b) is: "+(a>=b));
+		}
+	else
+		{
+			System.out.println("(a>=b) is: "+(a>=b));
+		}
+	if(a<=b)
+		{
+			System.out.println("(a<=b) is: "+(a<=b));
+		}
+	else
+		{
+			System.out.println("(a<=b) is: "+(a<=b));
+		}
+	}
+}
