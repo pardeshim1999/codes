2eb141d90403265ccea6d33b61dd0a37e08eed99
pardeshim1999 <pardeshim1999@gmail.com>
2020-12-15 21:11:46 +0530

    Create ifelse.cpp

---
+++ c++/ifelse.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a=80;
+
+	cout<<"Student Marks="<<a<<endl;
+
+	if(a>=75)
+	{
+		cout<<"Distinction"<<endl;
+	}
+	else if(a>=50)
+	{
+		cout<<"First Class"<<endl;
+	}
+	else if(a>=35)
+	{
+		cout<<"Second Class"<<endl;
+	}
+}
